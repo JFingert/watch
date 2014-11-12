6f3138f84d7cf45311a59d96489c70a053116410
JFingert <joshuafingert@gmail.com>
2014-11-12 10:14:08 -0800

    adds particles

--- app.js
+++ app.js
@@ -46,3 +46,39 @@ angular.module('watchApp', [])
 	});
 	
 });
+
+
+particlesJS('particles-js', {
+    particles: {
+        color: '#1638A3',
+        shape: 'circle',
+        opacity: 1,
+        size: 2.5,
+        size_random: true,
+        nb: 100,
+        line_linked: {
+            enable_auto: true,
+            distance: 250,
+            color: '#1638A3',
+            opacity: 0.5,
+            width: 1,
+            condensed_mode: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600
+            }
+        },
+        anim: {
+            enable: true,
+            speed: 2
+        }
+    },
+    interactivity: {
+        enable: true,
+        mouse: {
+            distance: 200
+        },
+        mode: 'grab'
+    },
+    retina_detect: true
+});
